7596a7ca27de7605d21a40ef49dc30c0f252f447
Susheela1199 <susheela161127@gmail.com>
2022-06-01 13:03:05 +0530

    first api practice

--- practice.py
+++ practice.py
@@ -4,3 +4,8 @@ from fastapi import FastAPI
 #pip install fastapi
 #pip install uvicorn[standard]
 #uvicorn pythonscriptname:app --reload
+app = FastAPI()
+
+@app.get("/")
+async def simple_api():
+    return {"FastAPI : This is first api"}
